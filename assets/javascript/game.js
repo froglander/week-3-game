5b90d11e2c5a8fe2476506f2d0e1ddcf0fabda0b
Kristi Heredia <froglander@dukworld.net>
2016-07-15 22:31:41 -0400

    Continued working on javascript game file and styling

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,17 +1,15 @@
 var hangman = {
-		word: ["Mario", "Luigi", "Peach", "Bowser", "Blooper", "Bullet Bill", "Buzzy Beetle", "Chain Chomp", "Dry Bones", "Goomba", "Hammer Brothers", "Koopa Troopa", "Lakitu", "Magikoopa", "Piranha Plant", "Spiny", "Thwomp"],
+		word: ["Mario", "Luigi", "Peach", "Bowser", "Blooper", "Bullet Bill", "Buzzy Beetle", "Chain Chomp", "Dry Bones", "Goomba", "Hammer Brothers", "Koopa Troopa", "Lakitu", "Magikoopa", "Piranha Plant", "Spiny", "Thwomp", "Yoshi"],
 		wins: 0,
 		losses: 0,
 		tries: 10, /* Start with 10 tries */
 		currentGuess: [], /* Blank array to store current guesses */
+		lettersGuessed: [], /* Blank array to store all guessed letters */
 		roundWord: "", /* to store the random word chosen for the round */
 		randomWord : function() {
 			var x = Math.floor((Math.random() * this.word.length));
 			return this.word[x];
-		},
-		displayWord: function(word) {
-			document.getElementById('showWord').innerHTML = word;
-		},
+		},		
 		/* **************************************************************** */
 		/*	Fill the currentGuess string array with a placeholder character	*/
 		/*	for the number of letters in the random word selected including	*/
@@ -24,7 +22,8 @@ var hangman = {
 				} else {
 					this.currentGuess.push("-");
 				}			
-			}	
+			}
+			$("#currentWord").html(this.currentGuess);	
 		},		
 		/* **************************************************************** */
 		/*	Check if letter that is passed matches a letter in word 		*/
@@ -57,36 +56,60 @@ var hangman = {
 		/* **************************************************************** */
 		playRound: function( event ) {
 			var letterChoice = String.fromCharCode(event.keyCode);	
+
+			// If letter has already been guessed, skip it			
+			if (this.lettersGuessed.indexOf(letterChoice) > -1 ) return; 
+			
 			var that = 	this;
 			var isMatch = that.checkLetter(letterChoice, this.roundWord);
-			
+
+			this.lettersGuessed.push(letterChoice);
+
 			if (isMatch.length > -1) {				
 				$.each(isMatch, function(i, match) {
 					that.currentGuess[match] = that.roundWord[isMatch[i]];
 				});
 			} 
-			$("#gameState").html(this.currentGuess);
+			$("#currentWord").html(this.currentGuess);
 			$("#triesLeft").html(this.tries);
-
+			$("#lettersGuessed").html(this.lettersGuessed.toString());
 
 			if (this.currentGuess.join("") == this.roundWord) {
-				$("#currentStatus").html("Yay, you guessed the word!");
+				$("#gameState").html("Yay, you guessed the word!");
 				this.wins++;
-				$("#wins").html("You have won " + this.wins + " games");
-				// call reset
+				$("#wins").html(this.wins);
+				this.gameReset();
 			} else if (this.tries == 0){
 				//Oh no, you lost!
-				$("#currentStatus").html("Oh no, you lost!");
+				$("#gameState").html("Oh no, you lost! <br/> The word was: " + this.roundWord);
 				this.losses++;
-				// call reset
+				$("#losses").html(this.losses);
+				this.gameReset();
 			} else {
-				$("#currentStatus").html("Keep guessing!");
+				$("#gameState").html("Keep guessing!");
 			}
+		},
+		/* **************************************************************** */
+		/* Reset function to get a new word and reset to blank guesses		*/
+		/* **************************************************************** */
+		gameReset: function() {
+			this.roundWord = this.randomWord();
+			this.currentGuess = [];
+			this.blankCurrentGuess();	
+			this.tries = 10;			
+			this.lettersGuessed = [];
+			$("#gameState").html("");
+			$("#triesLeft").html(this.tries);
+			$("#lettersGuessed").html(this.lettersGuessed.toString());			
 		}
 	}
 
 	hangman.roundWord = hangman.randomWord();	
 	hangman.blankCurrentGuess();
+	$("#triesLeft").html(hangman.tries);
+	$("#wins").html(hangman.wins);
+	$("#losses").html(hangman.losses);
+
 
 	// Courtesy of Dwight so it would work after I moved playRound into my object
 	$(document).on("keyup", hangman.playRound.bind(hangman));
